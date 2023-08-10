745a0727f44aaef89f8a9ef220b7b8d366929aee
Miles Johnson <mileswjohnson@gmail.com>
2023-08-10 14:53:47 -0700

    Add shim tests.

--- tests/shims_test.rs
+++ tests/shims_test.rs
@@ -7,3 +7,41 @@ generate_global_shims_test!(
     [],
     Some(locate_fixture("schemas").join("base.toml"))
 );
+
+#[tokio::test]
+async fn doesnt_create_global_shim() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_schema_plugin(
+        "schemas",
+        sandbox.path(),
+        locate_fixture("schemas/shim-no-global.toml"),
+    );
+
+    plugin.tool.create_shims(false).await.unwrap();
+
+    assert!(!sandbox.path().join(".proto/bin/schema-test").exists());
+}
+
+#[tokio::test]
+async fn can_create_local_shim() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_schema_plugin(
+        "schemas",
+        sandbox.path(),
+        locate_fixture("schemas/shim-local.toml"),
+    );
+
+    plugin.tool.create_shims(false).await.unwrap();
+
+    assert!(sandbox
+        .path()
+        .join(".proto/tools/schemas/latest/shims/schemas")
+        .exists());
+
+    assert_snapshot!(std::fs::read_to_string(
+        sandbox
+            .path()
+            .join(".proto/tools/schemas/latest/shims/schemas")
+    )
+    .unwrap());
+}
