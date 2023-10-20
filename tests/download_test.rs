7fc5c82e1a2480f8279a6cfef358eea8c6bb28a5
Miles Johnson <mileswjohnson@gmail.com>
2023-10-19 18:56:34 -0700

    Fix format.

--- tests/download_test.rs
+++ tests/download_test.rs
@@ -28,14 +28,14 @@ fn supports_linux_arm64() {
                 version: "20.0.0".into(),
                 ..Default::default()
             },
-                ..Default::default()
+            ..Default::default()
         }),
         DownloadPrebuiltOutput {
             archive_prefix: Some("moon-linux-aarch64-20.0.0".into()),
             checksum_name: Some("CHECKSUM.txt".into()),
             download_name: Some("moon-aarch64-unknown-linux-gnu".into()),
             download_url: "https://github.com/moonrepo/moon/releases/download/v20.0.0/moon-aarch64-unknown-linux-gnu".into(),
-                ..Default::default()
+            ..Default::default()
         }
     );
 }
@@ -61,14 +61,14 @@ fn supports_linux_x64() {
                 version: "20.0.0".into(),
                 ..Default::default()
             },
-                ..Default::default()
+            ..Default::default()
         }),
         DownloadPrebuiltOutput {
             archive_prefix: Some("moon-linux-x86_64-20.0.0".into()),
             checksum_name: Some("CHECKSUM.txt".into()),
             download_name: Some("moon-x86_64-unknown-linux-gnu".into()),
             download_url: "https://github.com/moonrepo/moon/releases/download/v20.0.0/moon-x86_64-unknown-linux-gnu".into(),
-                ..Default::default()
+            ..Default::default()
         }
     );
 }
@@ -94,14 +94,14 @@ fn supports_macos_arm64() {
                 version: "20.0.0".into(),
                 ..Default::default()
             },
-                ..Default::default()
+            ..Default::default()
         }),
         DownloadPrebuiltOutput {
             archive_prefix: None,
             checksum_name: Some("SHASUM256.txt".into()),
             download_name: Some("moon-aarch64-apple-darwin".into()),
             download_url: "https://github.com/moonrepo/moon/releases/download/v20.0.0/moon-aarch64-apple-darwin".into(),
-                ..Default::default()
+            ..Default::default()
         }
     );
 }
@@ -127,14 +127,14 @@ fn supports_macos_x64() {
                 version: "20.0.0".into(),
                 ..Default::default()
             },
-                ..Default::default()
+            ..Default::default()
         }),
         DownloadPrebuiltOutput {
             archive_prefix: None,
             checksum_name: Some("SHASUM256.txt".into()),
             download_name: Some("moon-x86_64-apple-darwin".into()),
             download_url: "https://github.com/moonrepo/moon/releases/download/v20.0.0/moon-x86_64-apple-darwin".into(),
-                ..Default::default()
+            ..Default::default()
         }
     );
 }
@@ -160,14 +160,14 @@ fn supports_windows_arm64() {
                 version: "20.0.0".into(),
                 ..Default::default()
             },
-                ..Default::default()
+            ..Default::default()
         }),
         DownloadPrebuiltOutput {
             archive_prefix: None,
             checksum_name: Some("CHECKSUM.txt".into()),
             download_name: Some("moon-aarch64-pc-windows-msvc.exe".into()),
             download_url: "https://github.com/moonrepo/moon/releases/download/v20.0.0/moon-aarch64-pc-windows-msvc.exe".into(),
-                ..Default::default()
+            ..Default::default()
         }
     );
 }
@@ -193,14 +193,14 @@ fn supports_windows_x86() {
                 version: "20.0.0".into(),
                 ..Default::default()
             },
-                ..Default::default()
+            ..Default::default()
         }),
         DownloadPrebuiltOutput {
             archive_prefix: None,
             checksum_name: Some("CHECKSUM.txt".into()),
             download_name: Some("moon-x86-pc-windows-msvc.exe".into()),
             download_url: "https://github.com/moonrepo/moon/releases/download/v20.0.0/moon-x86-pc-windows-msvc.exe".into(),
-                ..Default::default()
+            ..Default::default()
         }
     );
 }
